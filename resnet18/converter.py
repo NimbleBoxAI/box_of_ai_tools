415c6950be3a9b06927608449d6f10b5826165dc
Aakash Kaushik <kaushikaakash7539@gmail.com>
2021-08-04 15:53:14 +0530

    improved converter script

--- resnet18/converter.py
+++ resnet18/converter.py
@@ -1,25 +1,16 @@
+import os
 import nbox
 import json
-import os
 import torch 
+import warnings
 import subprocess
 import torchvision
 import numpy as np
 from openvino.inference_engine import IECore
 
+warnings.filterwarnings("ignore")
+
 export_model_name = "resnet18"
-int8_json_filename = export_model_name + ".json" 
-model_xml = "./" + export_model_name + "/" + export_model_name + "_FP32.xml"
-model_bin = "./" + export_model_name + "/" + export_model_name + "_FP32.bin"
-
-with open("int8_template.json", 'r') as f:
-    data = json.load(f)
-    data["model"]["model_name"] = export_model_name
-    data["model"]["model"] = model_xml
-    data["model"]["weights"] = model_bin
-
-with open(int8_json_filename, 'w') as f:
-    json.dump(data, f, indent=4)
 
 tensor_inp = torch.rand(50, 3, 300, 300)
 numpy_inp = tensor_inp.detach().numpy()
@@ -39,13 +30,28 @@ subprocess.run(['python3', '/opt/intel/openvino_2021.4.582/deployment_tools/mode
                 '--model_name', export_model_name + '_FP32'
                 ])
 
+model_xml = "./" + export_model_name + "/" + export_model_name + "_FP32.xml"
+model_bin = "./" + export_model_name + "/" + export_model_name + "_FP32.bin"
 
 ie = IECore()
 openvino_net = ie.read_network(model=model_xml, weights=model_bin)
 exec_net = ie.load_network(network=openvino_net, device_name="CPU", num_requests=4)
 openvino_out = exec_net.infer(inputs={"input": numpy_inp})
 
-print(torch.sum(torch_out).item())
-print("{:.12f}".format(np.sum(openvino_out['output'])))
+print("Torch out sum: ", torch.sum(torch_out).item())
+print("Openvino FP32 out sum:", "{:.12f}".format(np.sum(openvino_out['output'])))
+
+int8_json_filename = export_model_name + ".json" 
+
+with open("int8_template.json", 'r') as f:
+    data = json.load(f)
+    data["model"]["model_name"] = export_model_name
+    data["model"]["model"] = model_xml
+    data["model"]["weights"] = model_bin
+
+with open(int8_json_filename, 'w') as f:
+    json.dump(data, f, indent=4)
+
+print(int8_json_filename + " created for conversion to int8 format.")
 
 subprocess.run(['pot', '-c', int8_json_filename, '-d'])
