85d86c191306a35c33b3e9fecee0a96410e019eb
Aakash Kaushik <kaushikaakash7539@gmail.com>
2021-08-04 15:45:20 +0530

    int8_config file added

--- resnet18/converter.py
+++ resnet18/converter.py
@@ -1,14 +1,29 @@
 import nbox
+import json
+import os
 import torch 
 import subprocess
 import torchvision
 import numpy as np
 from openvino.inference_engine import IECore
 
+export_model_name = "resnet18"
+int8_json_filename = export_model_name + ".json" 
+model_xml = "./" + export_model_name + "/" + export_model_name + "_FP32.xml"
+model_bin = "./" + export_model_name + "/" + export_model_name + "_FP32.bin"
+
+with open("int8_template.json", 'r') as f:
+    data = json.load(f)
+    data["model"]["model_name"] = export_model_name
+    data["model"]["model"] = model_xml
+    data["model"]["weights"] = model_bin
+
+with open(int8_json_filename, 'w') as f:
+    json.dump(data, f, indent=4)
+
 tensor_inp = torch.rand(50, 3, 300, 300)
 numpy_inp = tensor_inp.detach().numpy()
 model = nbox.load("resnet18", True).get_model().eval()
-export_model_name = 'resnet18'
 torch_out = model(tensor_inp)
 
 torch.onnx.export(model,
@@ -24,8 +39,6 @@ subprocess.run(['python3', '/opt/intel/openvino_2021.4.582/deployment_tools/mode
                 '--model_name', export_model_name + '_FP32'
                 ])
 
-model_xml = "./" + export_model_name + "/" + export_model_name + "_FP32.xml"
-model_bin = "./" + export_model_name + "/" + export_model_name + "_FP32.bin"
 
 ie = IECore()
 openvino_net = ie.read_network(model=model_xml, weights=model_bin)
@@ -34,3 +47,5 @@ openvino_out = exec_net.infer(inputs={"input": numpy_inp})
 
 print(torch.sum(torch_out).item())
 print("{:.12f}".format(np.sum(openvino_out['output'])))
+
+subprocess.run(['pot', '-c', int8_json_filename, '-d'])
