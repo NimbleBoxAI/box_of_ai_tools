ceb14cb1bcc1d846b8dbc9517fb2eede6f617910
Aakash Kaushik <kaushikaakash7539@gmail.com>
2021-08-13 12:46:49 +0530

    refine int8 conversion

--- 50-models/resnet18/converter.py
+++ 50-models/resnet18/converter.py
@@ -31,7 +31,8 @@ warnings.filterwarnings("ignore")
 export_model_name = "resnet18"
 tensor_inp = torch.rand(1, 3, 224, 224)
 numpy_inp = tensor_inp.detach().numpy()
-tensor_inp = transform(tensor_inp)
+numpy_inp_int8 = tensor_inp.detach().numpy()
+# tensor_inp = transform(tensor_inp)
 
 model = nbox.load("resnet18", True).get_model().eval()
 torch_start_time = time.time()
@@ -48,9 +49,9 @@ torch.onnx.export(model,
 subprocess.run(['python3', '/opt/intel/openvino_2021.4.582/deployment_tools/model_optimizer/mo.py',
                 '--input_model', export_model_name + '.onnx',
                 '--output_dir', export_model_name,
-                '--model_name', export_model_name + '_FP32',
-                '--mean_values', '[123.675,116.28,103.53]',
-                '--scale_values', '[58.395,57.12,57.375]'
+                '--model_name', export_model_name + '_FP32'
+                # '--mean_values', '[123.675,116.28,103.53]',
+                # '--scale_values', '[58.395,57.12,57.375]'
                 ])
 
 model_xml = "./" + export_model_name + "/" + export_model_name + "_FP32.xml"
@@ -68,12 +69,6 @@ openvino_start_time = time.time()
 openvino_out = exec_net.infer(inputs={"input": numpy_inp})
 openvino_end_time = time.time()
 
-print("\nPyTorch out sum: ", torch.sum(torch_out).item())
-print("OpenVINO FP32 out sum:", "{:.12f}".format(np.sum(openvino_out['output'])))
-print("Time taken to run PyTorch inference: ", torch_end_time - torch_start_time)
-print("Time taken to run OpenVINO inference: ", openvino_end_time - openvino_start_time) 
-
-
 int8_json_filename = export_model_name + ".json" 
 
 with open("int8_template.json", 'r') as f:
@@ -88,3 +83,20 @@ with open(int8_json_filename, 'w') as f:
 # Int8 conversion script - some errors to iron out.
 print(int8_json_filename + " created for conversion to int8 format.\n")
 subprocess.run(['pot', '-c', int8_json_filename, '-d'])
+
+model_xml_int8 = "./results/optimized" + "/" + export_model_name + ".xml"
+model_bin_int8 = "./results/optimized" + "/" + export_model_name + ".bin"
+
+ie_int8 = IECore()
+openvino_net_int8 = ie_int8.read_network(model=model_xml_int8, weights=model_bin_int8)
+exec_net_int8 = ie_int8.load_network(network=openvino_net_int8, device_name="CPU", num_requests=4)
+openvino_int8_start_time = time.time()
+openvino_out_int8 = exec_net_int8.infer(inputs={"input": numpy_inp_int8})
+openvino_int8_end_time = time.time()
+
+print("\nPyTorch out sum: ", torch.sum(torch_out).item())
+print("OpenVINO FP32 out sum:", "{:.12f}".format(np.sum(openvino_out['output'])))
+print("OpenVINO INT8 out sum:", "{:.12f}".format(np.sum(openvino_out_int8['output'])))
+print("Time taken to run PyTorch inference: ", torch_end_time - torch_start_time)
+print("Time taken to run OpenVINO FP32 inference: ", openvino_end_time - openvino_start_time) 
+print("Time taken to run OpenVINO INT8 inference: ", openvino_int8_end_time - openvino_int8_start_time)
